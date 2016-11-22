550e352eccf4e7c5b7e0ed8f9414c749f8a8871b
Gagoi <Gagoi@192.168.1.75>
2016-11-22 20:53:48 +0100

    Optimisation de l'utilisation de la librairie.

--- fr/gagoi/Launcher.java
+++ fr/gagoi/Launcher.java
@@ -3,7 +3,7 @@ package fr.gagoi;
 public class Launcher {
 
 	public static void main(String[] args) {
-		new Thread(new fr.gagoi.basic_racer.Game()).start();
+		new fr.gagoi.basic_racer.Game();
 		//new Thread(new fr.gagoi.snow.Game()).start();
 	}
 
--- fr/gagoi/basic_racer/Game.java
+++ fr/gagoi/basic_racer/Game.java
@@ -5,48 +5,21 @@ import fr.gagoi.pwal.graphics.window.Display;
 import fr.gagoi.pwal.graphics.window.Layer;
 import fr.gagoi.pwal.inputs.Keyboard;
 
-public class Game implements Runnable {
+public class Game  {
 
 	private Display d = new Display("Basic Racer", 186, 600, 60);
 	private Application app = new Application("Basic Racer", 120);
 	private Layer game = new Layer(d, 0, 0);
-	private Keyboard keyboard = new Keyboard();
 	private PlayerCar player = new PlayerCar(0);
 	
 	// B = Bordure, T = Ligne blanche, V = Voie;
 	// B-T-V-T-V-T-V-T-B
 	// 40-4-30-4-30-4-30-4-40
-	private World w = new World(player, keyboard);
+	private World w = new World(player, d.getKeyboard());
 	
 	public Game() {
 		d.getLayers().add(game);
 		game.add(w);
-		d.addKeyListener(keyboard);
 		app.add(w);
-	}
-
-
-	@Override
-	public void run() {
-		long x = 0;
-		while (true) {
-			if (x % 1_000_000 == 0){
-//				
-//				for (int i = 0; i < snows.size(); i++) {
-//					Snow s = snows.get(i);
-//					if(s.getPosY() >= 1000) {
-//						snows.remove(s);
-//						app.remove(s);
-//						game.remove(s);
-//					}
-//				}
-//				
-//				Snow s = new Snow();
-//				snows.add(s);
-//				app.add(s);
-//				game.add(s);
-			}
-			x++;
-		}
 	}
 }
